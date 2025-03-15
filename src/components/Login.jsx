19aab32aedd598cb2b6eac2bad61a5bf3ec99761
Marush Denchev <avreon@gmail.com>
2025-03-15 06:32:55 +0200

    Sign in with Google and logout

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,13 +1,33 @@
 import { useState } from "react";
-import { auth } from "../config/firebase";
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth, googleProvider} from "../config/firebase";
+import { createUserWithEmailAndPassword, signInWithPopup, signOut} from "firebase/auth";
 
 export default function Login() {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
 
     const signIn = async () => {
-        await createUserWithEmailAndPassword(auth, email, password)
+        try {
+            await createUserWithEmailAndPassword(auth, email, password)
+        } catch (e) {
+            console.error(e.message);
+        }
+    }
+
+    const signInWithGoogle = async () => {
+        try {
+            await signInWithPopup(auth, googleProvider)
+        } catch (e) {
+            console.error(e.emessage);
+        }
+    }
+
+    const logout = async () => {
+        try {
+            await signOut(auth);
+        } catch (e) {
+            console.error(e.message);
+        }
     }
     return (
         <>
@@ -16,6 +36,8 @@ export default function Login() {
                 <input type="text" placeholder="Email" onChange={(event) => {setEmail(event.target.value)}} />
                 <input type="password" placeholder="Password" onChange={(event) => {setPassword(event.target.value)}} />
                 <button type="button" onClick={signIn}>Вход</button>
+                <button type="button" onClick={signInWithGoogle}>Вход с Google</button>
+                <button type="button" onClick={logout}>Изход</button>
             </form>
         </>
     );
--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -14,4 +14,5 @@ const firebaseConfig = {
   // Initialize Firebase
   const app = initializeApp(firebaseConfig);
   export const auth = getAuth(app);
+  export const googleProvider = new GoogleAuthProvider(app);
   
